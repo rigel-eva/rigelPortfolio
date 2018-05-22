69399546ec42aa37a843ce8dfb5bb148ab4ec381
Nora Maguire <eva@rigel.moe>
2018-05-22 15:17:36 -0500

    Welp, about damn time I started working on this again

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'landing_page#welcome'
   get '/wordpress_preview', to: 'wordpress_preview#index'
   get '/github', to: 'github#index'
+  get 'signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
