7b5c20402e045d73c56cc02564c9f1c033660f87
Nora Maguire <eva@rigel.moe>
2018-06-06 21:15:02 -0500

    small commmit, defining project a bit further

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 describe Project, :type => :model do
     before(:all) do
         @project=Project.new
+        puts @project
     end
     context "Given Valid data" do
-        describe "is valid"
+        describe "is valid" do
+            @project.projectName=Forgery(:lorem_ipsum).words(2)
+            @project.projectStartDate=Forgery(:date).date
+            @project.projectEndDate=Forgery(:date).date
+            @project.projectInfo=Forgery(:lorem_ipsum).paragraphs(3)
+            @project.current=false
+            @project.client=Forgery(:name).company_name
+            expect{@project.save!}.to_not raise_error
+        end
     end
     context "Given Missing data" do
         describe "is not valid without a name"
