9b6397de9ddabdfce2004916d10f38e97c633087
Nora Maguire <eva@rigel.moe>
2018-06-12 20:13:00 -0500

    Ok a few changes: *Updated the Dockerfile to add getting yarn (should have found out about that earlier) *Updated the rails bin *Updated Production & Assets to add assets (hopefully)

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM ruby:2.3.3
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+
+RUN curl -sL https://deb.nodesource.com/setup_6.x | bash - && apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /rigelPortfolio
 WORKDIR /rigelPortfolio
 COPY Gemfile /rigelPortfolio/Gemfile
 COPY Gemfile.lock /rigelPortfolio/Gemfile.lock
+RUN npm install yarn -g
 RUN bundle install
 COPY . /rigelPortfolio
 RUN RAILS_ENV=production rails assets:precompile
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.precompile =  ['*.*'] 
+  config.assets.compile = false
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -40,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
+  # config.action_cable.mount_path = nil~
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
